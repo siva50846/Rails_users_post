09dbd60f605651494e3420cd9e974763f5a919e0
sivaram <sivaramprasad.it@gmail.com>
2012-08-16 06:12:34 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsUsersPost::Application.config.secret_token = '5ed82132212f74c53c9e6112bd585f6d99a9ee16c23fc75a46f825739844614e0c187e908fc1e6c58566c0502b2b23380a80dda7814e5d1ce8c9d2e97580fbdd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsUsersPost::Application.config.session_store :cookie_store, key: '_Rails_users_post_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsUsersPost::Application.config.session_store :active_record_store
